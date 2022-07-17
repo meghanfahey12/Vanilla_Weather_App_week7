5b659ddcd49b5a780f1990df764e5befab4c0f65
meghanfahey12 <108822384+meghanfahey12@users.noreply.github.com>
2022-07-17 14:20:33 -0400

    added form for search engine function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
+function formatDate(timestamp) {
+  let date= new Date(timestamp);
+  let hours= date.getHours();
+  if (hours = < 10){
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -17,9 +17,57 @@ function formatDate(date) {
     "Friday",
     "Saturday"
   ];
-  let day = days[dayIndex];
-  return `${day} ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
 }
+
+function displayTemperature(response){
+let temperatureElement = document.querySelector("temperature");
+let cityElement = document.querySelector("city");
+let descriptionElement = document.querySelector("description");
+let humidityElement = document.querySelector("humidity");
+let windElement = document.querySelector("wind");
+let dateElement = document.querySelector("date");
+let iconElement= document.querySelector("icon");
+
+temperatureElement.innerHTML= Math.round (response.data.main.temp);
+cityElement.innerHTML= response.data.name;
+descriptionElement.innerHTML= response.data.weather(#).description;
+humidityElement.innerHTML= response.data.main.humidity;
+windElement.innerHTML= Math.round(response.data.main.speed);
+dateElement.innerHTML= formatDate(response.data.dt*1000);
+iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+
+iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let city= "New York";
+  let apiUrl = `https://api.openweather.org/data/2.5/weather?
+  q=New York&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+  
+
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+
+
+let form= document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
+
+
+
+
+
+
 
 function displayWeatherCondition(response) {
   console.log(response);
