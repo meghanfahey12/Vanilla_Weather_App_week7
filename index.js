2060f45abc39d8c28b2dd56c1e04c1c17e8d9ec7
meghanfahey12 <108822384+meghanfahey12@users.noreply.github.com>
2022-07-27 13:07:12 -0400

    fix axios for city

--- index.js
+++ index.js
@@ -95,8 +95,10 @@ getForecast(response.data)
 
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;  axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweather.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
+
 
 function handleSubmit(event){
   event.preventDefault();
