149f004cc4d8c16a58c89900b8b8b1b4dc7917b4
meghanfahey12 <108822384+meghanfahey12@users.noreply.github.com>
2022-08-09 16:00:24 -0400

    fixed extra div

--- index.js
+++ index.js
@@ -31,35 +31,40 @@ function formatDay(timestamp) {
 
 
 function displayForecast(response) {
-  let forecast = response.data.daily; 
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
-  
-  let forecastHTML = `<div class ="row">`;  
+
+  let forecastHTML = `<div class ="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
-forecastHTML = forecastHTML + `
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-2">
     <div class="weather-forecast-date">
-    ${formatDay(forecastDay.dt)}</div>
+    ${formatDay(forecastDay.dt)}
+    </div>
     <img 
-    src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+    src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png" 
     alt=""
     width="42"/>
-      <div class=" weather-forecast-temperature">
+    <div class=" weather-forecast-temperature">
       <span class="weather-forecast.temperature-max">
       ${Math.round(forecastDay.temp.max)}°
       </span>
       <span class="weather-forecast.temperature-min">
       ${Math.round(forecastDay.temp.min)}°
       </span>
-      </div>
-   </div>
+    </div>
   </div>`;
-    };})
+    }
+  });
 
-forecastHTML = forecastHTML + `</div>`;
-forecastElement.innerHTML = forecastHTML;
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
   function getForecast(coordinates) {
